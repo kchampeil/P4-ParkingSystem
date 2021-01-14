44e9119cb15261ef45728ce7f59d918b450b6d13
Karine CHAMPEIL <kchampeilpro@gmail.com>
2021-01-14 15:10:23 +0100

    testParkingLotExit implemented intermediate commit before implementing DateUtil class

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -7,18 +7,17 @@ import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
 import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
@@ -55,6 +54,7 @@ public class ParkingDataBaseIT {
     }
 
     @Test
+    @DisplayName("Given an incoming vehicle when the incoming process is finished then a ticket is actually saved in DB and the parking table is updated with availability")
     public void testParkingACar() {
 
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
@@ -75,12 +75,36 @@ public class ParkingDataBaseIT {
     }
 
     @Test
+    @DisplayName("Given an exiting vehicle when the exiting process is finished then, in the DB, the ticket has been updated with calculated fare and out time, and parking spot is set to free")
     public void testParkingLotExit() {
+        // initialize the test with an incoming car
         testParkingACar();
+        Ticket savedTicket = ticketDAO.getTicket(VEHICLE_REG_NUMBER_FOR_TESTS);
+        System.out.println("saved ticket id: " + savedTicket.getId());
+        System.out.println("saved ticket out time: " + savedTicket.getOutTime());
+
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processExitingVehicle();
+
         //TODO: check that the fare generated and out time are populated correctly in the database
-        //TODO-H : check that parking spot is set to free after exit (parking table is updated with availability)
+        //TODO-M vérifier la requête SQL du getTicket (ne prend qu'un retour mais sans autre critère que le n° d'immat ?
+        Ticket updatedTicket = ticketDAO.getTicket(VEHICLE_REG_NUMBER_FOR_TESTS);
+        assertNotNull(updatedTicket.getOutTime());
+        //TOASK il faut mocker l'objet Date pour gérer la durée entre in et out ?
+        assertNotEquals(0.0, updatedTicket.getPrice());
+
+        /*
+        System.out.println("updated ticket id: " + updatedTicket.getId());
+        System.out.println("updated ticket out time:" + updatedTicket.getOutTime());
+        System.out.println("updated ticket price: " + updatedTicket.getPrice());
+        */
+
+        // check that parking spot is set to free in DB after exit
+        // ie parkingSpot of the updated ticket is the next available slot for this parking type
+        int updatedParkingSpotId = updatedTicket.getParkingSpot().getId();
+        int nextAvailableSlot = parkingSpotDAO.getNextAvailableSlot(updatedTicket.getParkingSpot().getParkingType());
+        assertEquals(updatedParkingSpotId, nextAvailableSlot);
+
     }
 
 }
